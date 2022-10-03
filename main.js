5827d2cee0ea53b9d2d5c84a94b5035374fb2879
gabrilgvbf <87611035+gabrilgvbf@users.noreply.github.com>
2022-10-03 19:47:56 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,22 @@
+const form = document.getElementById('formNumeros');
+const numeroA=document.querySelector('.nro-a')
+
+const numeroB=document.querySelector('.nro-b')
+
+let resultado=false
+
+function verificar ( nA, nB ){
+    let verifique=nA>nB
+
+    return verifique
+}
+form.addEventListener('submit', function(e){
+        resultado= verificar (numeroA.valueAsNumber,numeroB.valueAsNumber)
+    e.preventDefault();
+    if (!resultado){
+alert(' POSITIVO - DENTRO DO ESPERADO')
+    }else{
+        alert('NEGATIVO -  FORA DO ESPERADO') 
+    }
+}
+)
