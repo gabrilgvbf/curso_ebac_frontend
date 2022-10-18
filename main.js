7a6297e46c9eb4821b4df0563d983b72de1d8b39
gabrilgvbf <87611035+gabrilgvbf@users.noreply.github.com>
2022-10-17 23:55:15 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+    this.preventDefault;
+
+    $('#campo-cpf').mask('000.000.000-00')
+
+
+    $('#campo-telefone').mask('(00) 00000-0000')
+
+    
+    $('#campo-cep').mask('00000-000')
+})
