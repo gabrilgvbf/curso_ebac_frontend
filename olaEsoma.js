ebe46395d98a99b9866dc07dafa1fa4ec41f868d
gabrilgvbf <87611035+gabrilgvbf@users.noreply.github.com>
2023-02-14 19:48:22 -0300

    Add files via upload

---
+++ olaEsoma.js
@@ -0,0 +1,5 @@
+"use strict";
+const dizOla = (nome) => (console.log('OLA ' + nome));
+const multDoisNumeros = (n1, n2) => (console.log(n1 * n2));
+dizOla('GABRIEL');
+multDoisNumeros(5, 5);
---
+++ olaEsoma.ts
@@ -0,0 +1,5 @@
+const dizOla = (nome: string) => (console.log('OLA ' + nome))
+const multDoisNumeros = (n1: number, n2: number) => (console.log(n1 * n2))
+
+dizOla('GABRIEL')
+multDoisNumeros(5, 5)
